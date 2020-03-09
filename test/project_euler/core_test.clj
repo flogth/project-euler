e2f80eb4fd00e2f489906cf38a5d11aaa343fd03
rostrotkupferbraunbronze <f.guthmann@mailbox.org>
2020-03-09 21:20:57 +0100

    Add tests

--- test/project_euler/core_test.clj
+++ test/project_euler/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [project-euler.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-problem1
+  (testing "p1"
+    (is (= 233168 (problem1)))))
+
+(deftest test-problem2
+  (testing "p2"
+    (is (= 4613732 (problem2)))))
+
+(deftest test-problem3
+  (testing "p3"
+    (is (= 6857 (problem3)))))
